12ae071224211e679ea26f58929954f833c1b43d
AlBarbe <alessio.barberi.2501@gmail.com>
2025-02-11 15:18:12 +0100

    Removing the buttton to set the trigger level, implementing pab.h file wich contain the class to perform an auto regulation of trigger level

---
+++ include/pab.h
@@ -0,0 +1,74 @@
+#ifndef __pab__
+#define __pab__
+
+#include <Arduino.h>
+
+class Trigger {
+    public:
+        Trigger(int PinTouch) {
+            PIN = PinTouch;   
+        }
+        virtual ~Trigger() { ; };
+
+        void initialize () {
+            for (int i = 0; i < 10; i++)
+                readings[i] = touchRead(PIN);
+            mean = FindMean(readings);
+            trig = round(FindTrig(readings, mean));
+        }
+
+        void debug () {
+            Serial.printf("Trigger: %d - Mean: %f - Array: [ ", trig, mean);
+            for (int i = 0; i < 10; i++) Serial.printf("%d ", readings[i]);
+            Serial.printf("] - Touched: %s\n", TOUCHED?"true":"false" );
+        }
+
+        void update (int GetValue) {
+            if (GetValue < trig) {
+                TOUCHED = true;
+            }
+            else {
+                for (int i = 9; i > 0; i--) {
+                    readings[i] = readings[i - 1];
+                }
+                readings[0] = GetValue;
+                TOUCHED = false;
+            }
+            mean = FindMean(readings);
+            trig = round(FindTrig(readings, mean));
+        }
+
+        bool touch () {
+            return TOUCHED;
+        }
+
+    private:
+        int PIN;
+        int VALUE = -1;
+        bool TOUCHED = false;
+        int trig = -1;
+        float mean = -1;
+        int readings[10] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        float FindMean (int ARRAY [10]) {
+            int sum = 15;
+            for (int i = 0; i < 10; i++) {
+                sum += ARRAY[i];
+            }
+            float media = (float)sum / 10.0;
+            return media;
+        }
+
+        float FindTrig (int ARRAY[10], int MEAN) {
+            float MaxError = 0;
+            for (int i = 0; i < 10; i++) {
+                float ERROR = abs(MEAN - ARRAY[i]);
+                if ( ERROR > MaxError) {
+                    MaxError = ERROR;
+                }
+            }
+            return MEAN-MaxError-1.5;
+        }
+};
+
+#endif //__pab__
--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,44 @@
 #include <Arduino.h>
+#include "pab.h"
 
-#define PIN_LED     22    //GPIO used as led output signal
-#define BUTTON_PIN  35    //GPIO for the button used to reset trigger level
+//class declaration
+
+#define PIN_INPUT     T0    //GPIO used as input signal - must chose between touch pin of the board (EX: T0, T1, etc..)
 
 int value = 0;
-int trigger = 65;
+Trigger trigger (PIN_INPUT);
 bool touched1 = false;
 bool touched2 = false;
-int lastState = HIGH;     // the previous state from the input pin
-int currentState;
-bool LightUp = true;
 
 void setup(){
   Serial.begin(115200);
+  delay (2000);
 
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(PIN_LED, OUTPUT);
-  digitalWrite(PIN_LED, LOW);
-  {
-    Serial.println("Setiing trigger level");
-    int min = 100;                        //max value that touch can output
-    for (int i = 0; i < 30; i++) {        //this cycle seek for the lowes value
-      value = touchRead(T0);              
-      if (value < min) {
-        min = value;
-      }
-    }
-    trigger = min - 7;                    //set trigger n times lower than value
-    Serial.print("trigger set to:  ");
-    Serial.println(trigger);
-  }
+  trigger.initialize();
+  Serial.printf("Initialized as follow:");
+  trigger.debug();
+  Serial.printf("\n");
 }
 
 void loop(){
-
+ 
   value = touchRead(T0);
-  currentState = digitalRead(BUTTON_PIN);
-  //Serial.println(currentState);         //if you need debug in console
-  //Serial.println(value);
-
-  //Button to change Trigger level
-  if(lastState == LOW && currentState == HIGH){
-    Serial.println("The state changed from LOW to HIGH");
-
-    int min = 100;                        //max value that touch can output
-    for (int i = 0; i < 30; i++) {        //this cycle seek for the lowes value
-      value = touchRead(T0);              
-      if (value < min) {
-        min = value;
-      }
-    }
-    trigger = min - 7;                    //set trigger n times lower than value
-    Serial.print("trigger set to:  ");
-    Serial.println(trigger);
-  }
+  trigger.update(value);
+  Serial.println(value);
+  trigger.debug();
 
   //The properly touch button and what to do when touched
-  if (value < trigger) {                  //check if being touched 
+  /*if (value < trigger) {                  //check if being touched 
     touched1 = true;
   } else {
     touched1 = false;
   }
   if (touched1 == true && touched2 == false)  //Do action on touch
   {
-    LightUp = !LightUp;
-    if (LightUp == false) {
-      for(int dutyCycle = 0; dutyCycle <= 80; dutyCycle++){   
-        // changing the LED brightness with PWM
-        analogWrite(PIN_LED, dutyCycle);
-        delay(3);
-      }
-      for(int dutyCycle = 80; dutyCycle <= 255; dutyCycle++){   
-        // changing the LED brightness with PWM
-        analogWrite(PIN_LED, dutyCycle);
-        delay(2);
-      }
-    }
-    else if (LightUp == true) {
-      for(int dutyCycle = 255; dutyCycle >= 80; dutyCycle--){
-        // changing the LED brightness with PWM
-        analogWrite(PIN_LED, dutyCycle);
-        delay(2);
-      }
-      for(int dutyCycle = 80; dutyCycle >= 0; dutyCycle--){
-        // changing the LED brightness with PWM
-        analogWrite(PIN_LED, dutyCycle);
-        delay(3);
-      }
-    }
-  }
+    
+  //--------------actions to do-------------------
 
-  lastState = currentState;
-  touched2 = touched1;
-  delay(100);
+  }
+  touched2 = touched1;*/
+  delay(1000);
 }
